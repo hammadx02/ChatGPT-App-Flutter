fe3eb7656ac725920e8e86e1149e22f520dcef1d
hammadx02 <hammadaali02@gmail.com>
2023-08-28 19:40:35 +0500

    updated

--- lib/views/chat_page.dart
+++ lib/views/chat_page.dart
@@ -8,11 +8,23 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final _textController = TextEditingController();
+  late bool isLoading;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    isLoading = false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor:
+              Theme.of(context).colorScheme.onBackground.withOpacity(0.1),
           centerTitle: true,
           automaticallyImplyLeading: false,
           elevation: 2,
@@ -28,6 +40,15 @@ class _ChatPageState extends State<ChatPage> {
               Expanded(
                 child: _buildList(),
               ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    _buildInput(),
+                    _buildSubmit(),
+                  ],
+                ),
+              )
             ],
           ),
         ),
@@ -35,7 +56,39 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
+  Widget _buildSubmit() {
+    return Visibility(
+      child: IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.send_rounded),
+      ),
+    );
+  }
+
+  Expanded _buildInput() {
+    return Expanded(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(90),
+        child: TextField(
+          enabled: !isLoading,
+          controller: _textController,
+          textCapitalization: TextCapitalization.sentences,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor:
+                Theme.of(context).colorScheme.onBackground.withOpacity(0.1),
+            border: InputBorder.none,
+            hintText:
+                isLoading ? 'Loading . . .' : 'Write something here . . .',
+          ),
+        ),
+      ),
+    );
+  }
+
   ListView _buildList() {
-    return ListView.builder(itemBuilder: (context, index) {});
+    return ListView.builder(
+      itemBuilder: (context, index) {},
+    );
   }
 }
